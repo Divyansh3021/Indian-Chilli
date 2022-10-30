c37cadcac45d0136c495e05a02f03da4503ee4d2
Divyansh3021 <divyansh3021@gmail.com>
2022-10-31 00:00:49 +0530

    second commit

--- src/components/RedeemCards.jsx
+++ src/components/RedeemCards.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import scheme from"./scheme.json"
 import "./CSS/redeem.css"
 
-function RedeemCards(props) {
+function RedeemCards() {
   return (
     <div className='redeem-box'>
         {
             scheme && scheme.map( times => {
                 return(
-                    <div className="redeem-card" style={{display: props.mode}} key={times.visit}>
+                    <div className="redeem-card" key={times.visit}>
+                        <h2>{times.visit}</h2>
                         {times.dishes && times.dishes.map(dish => {
                             return(
-                                <h2>{dish.dish}</h2>
+                                <div className="redeem-dish" style={{background: "url("+dish.url+")" , backgroundSize: "cover", }}>
+                                    <h2>{dish.dish}</h2>
+                                    <div className="redeem">Redeem</div>
+                                </div>
                             )
                         }
                         )}
